d14042201d59cd48199c21b883757a723ae8dee8
Choc12 <jannetgarcia154@gmail.com>
2023-06-25 13:54:16 -0500

    Make it so the window keeps running.

--- PyTux.py
+++ PyTux.py
@@ -4,3 +4,9 @@ import pygame
 print("Welcome to PyTux!")
 
 pygame.init(), pygame.display.set_mode((640, 480)), pygame.display.set_caption("PyTux")
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
