52b895b2571b210f31f506398c53a05feb41e1f6
Choc12 <jannetgarcia154@gmail.com>
2023-06-25 15:32:36 -0500

    Made window resizable

--- PyTux.py
+++ PyTux.py
@@ -1,12 +1,14 @@
 # import pygame
 import pygame
+import sys
 
 print("Welcome to PyTux!")
-
-pygame.init(), pygame.display.set_mode((640, 480)), pygame.display.set_caption("PyTux")
+print("PyTux is a Pygame-based game for Linux.")
+pygame.init(), pygame.display.set_mode((640, 480), pygame.RESIZABLE), pygame.display.set_caption("PyTux")
 pygame.display.flip(), pygame.display.update(), pygame.display.set_icon(pygame.image.load("images/icon.png"))
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+pygame.quit()
