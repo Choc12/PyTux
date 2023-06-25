c9f197e2afa23ed089f6b10b5f616e4abf2f7995
Choc12 <jannetgarcia154@gmail.com>
2023-06-25 14:20:04 -0500

    Added icon image

--- PyTux.py
+++ PyTux.py
@@ -4,7 +4,7 @@ import pygame
 print("Welcome to PyTux!")
 
 pygame.init(), pygame.display.set_mode((640, 480)), pygame.display.set_caption("PyTux")
-pygame.display.flip()
+pygame.display.flip(), pygame.display.update(), pygame.display.set_icon(pygame.image.load("images/icon.png"))
 running = True
 while running:
     for event in pygame.event.get():
